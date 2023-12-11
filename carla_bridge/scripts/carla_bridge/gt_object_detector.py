ba4955a7999ff99eb33ed0d199cc34af4269b46e
Andrea Turrini <andrea.turrini@gmail.com>
2023-12-11 15:55:09 +0800

    Split transformations

--- carla_bridge/scripts/carla_bridge/gt_object_detector.py
+++ carla_bridge/scripts/carla_bridge/gt_object_detector.py
@@ -1,30 +1,11 @@
 import numpy as np
-import math
 
 from iss_msgs.msg import ObjectDetection3DArray, ObjectDetection3D
 
 from planning_utils.angle import zero_2_2pi
 
-def quaternion_from_euler(self, ai, aj, ak):
-    ai /= 2.0
-    aj /= 2.0
-    ak /= 2.0
-    ci = math.cos(ai)
-    si = math.sin(ai)
-    cj = math.cos(aj)
-    sj = math.sin(aj)
-    ck = math.cos(ak)
-    sk = math.sin(ak)
-    cc = ci*ck
-    cs = ci*sk
-    sc = si*ck
-    ss = si*sk
-    q = np.empty((4, ))
-    q[0] = cj*sc - sj*cs
-    q[1] = cj*ss + sj*cc
-    q[2] = cj*cs - sj*sc
-    q[3] = cj*cc + sj*ss
-    return q
+from planning_utils.transformations import quaternion_from_euler
+
 
 class GTObjectDetector:
     def __init__(self, node, vehicle_id, world) -> None:
